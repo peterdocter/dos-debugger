397cede624ac0f7ee59b04a5660bc4b7fc3c4fd6
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-04-23 09:18:45 +0000

    Mostly migrated to DockPanel-based GUI.

---
+++ DotNet/DosDebugger/ListingWindow.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DosDebugger
+{
+    partial class ListingWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lvListing = new Util.Forms.DoubleBufferedListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuListing = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuListingGoToXRef = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuListing.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvListing
+            // 
+            this.lvListing.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvListing.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvListing.ContextMenuStrip = this.contextMenuListing;
+            this.lvListing.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvListing.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvListing.FullRowSelect = true;
+            this.lvListing.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvListing.HideSelection = false;
+            this.lvListing.Location = new System.Drawing.Point(0, 0);
+            this.lvListing.MultiSelect = false;
+            this.lvListing.Name = "lvListing";
+            this.lvListing.Size = new System.Drawing.Size(495, 270);
+            this.lvListing.TabIndex = 1;
+            this.lvListing.UseCompatibleStateImageBehavior = false;
+            this.lvListing.View = System.Windows.Forms.View.Details;
+            this.lvListing.VirtualMode = true;
+            this.lvListing.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvListing_RetrieveVirtualItem);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Address";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Opcode";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Disassembly";
+            this.columnHeader3.Width = 380;
+            // 
+            // contextMenuListing
+            // 
+            this.contextMenuListing.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuListingGoToXRef});
+            this.contextMenuListing.Name = "contextMenuListing";
+            this.contextMenuListing.Size = new System.Drawing.Size(177, 28);
+            this.contextMenuListing.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuListing_Opening);
+            // 
+            // mnuListingGoToXRef
+            // 
+            this.mnuListingGoToXRef.Name = "mnuListingGoToXRef";
+            this.mnuListingGoToXRef.Size = new System.Drawing.Size(176, 24);
+            this.mnuListingGoToXRef.Text = "Go to Reference";
+            // 
+            // ListingWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 270);
+            this.Controls.Add(this.lvListing);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.071428F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "ListingWindow";
+            this.Text = "Disassembly";
+            this.contextMenuListing.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Util.Forms.DoubleBufferedListView lvListing;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuListing;
+        private System.Windows.Forms.ToolStripMenuItem mnuListingGoToXRef;
+    }
+}
---
+++ DotNet/DosDebugger/ListingWindow.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Disassembler;
+using X86Codec;
+
+namespace DosDebugger
+{
+    public partial class ListingWindow : WeifenLuo.WinFormsUI.Docking.DockContent
+    {
+        public ListingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Document document;
+        private ListingViewModel listingView;
+
+        internal Document Document
+        {
+            get { return this.document; }
+            set
+            {
+                this.document = value;
+                UpdateUI();
+            }
+        }
+
+        public void UpdateUI()
+        {
+            lvListing.VirtualListSize = 0;
+            if (document == null)
+                return;
+
+            listingView = new ListingViewModel(document.Disassembler);
+            lvListing.VirtualListSize = listingView.Rows.Count;
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                if (lvListing.SelectedIndices.Count > 0)
+                    return lvListing.SelectedIndices[0];
+                else
+                    return -1;
+            }
+        }
+
+        private void lvListing_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = listingView.CreateViewItem(e.ItemIndex);
+        }
+
+        private void contextMenuListing_Opening(object sender, CancelEventArgs e)
+        {
+            mnuListingGoToXRef.DropDownItems.Clear();
+            mnuListingGoToXRef.Enabled = false;
+
+            int index = SelectedIndex;
+            if (index == -1)
+                return;
+
+            Pointer location = listingView.Rows[index].Location;
+            if (location == Pointer.Invalid)
+                return;
+
+            foreach (XRef xref in document.Disassembler.GetReferencesTo(location))
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Text = xref.Source.ToString();
+                item.Click += mnuListingGoToXRefItem_Click;
+                item.Tag = xref.Source;
+                mnuListingGoToXRef.DropDownItems.Add(item);
+            }
+            mnuListingGoToXRef.Enabled = mnuListingGoToXRef.HasDropDownItems;
+        }
+
+        private void mnuListingGoToXRefItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+            Pointer source = (Pointer)item.Tag;
+            NavigateTo(source);
+        }
+
+        private LinkedList<int> navHistory = new LinkedList<int>();
+        private LinkedListNode<int> navCurrent = null;
+
+        public bool NavigateBackward()
+        {
+             if (navCurrent != null && navCurrent.Previous != null)
+            {
+                navCurrent = navCurrent.Previous;
+                GoToRow(navCurrent.Value);
+                 return true;
+            }
+            return false;
+        }
+
+        public bool NavigateForward()
+        {
+            if (navCurrent != null && navCurrent.Next != null)
+            {
+                navCurrent = navCurrent.Next;
+                GoToRow(navCurrent.Value);
+                return true;
+            }
+            return false;
+        }
+
+        public bool NavigateTo(Pointer target)
+        {
+            // Find the first entry that is greater than or equal to target.
+            for (int i = 0; i < listingView.Rows.Count; i++)
+            {
+                Pointer current = listingView.Rows[i].Location;
+                if (current != Pointer.Invalid &&
+                    current.EffectiveAddress >= target.EffectiveAddress)
+                {
+                    GoToRow(i, true);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void GoToRow(int index, bool bringToTop = false)
+        {
+            ListViewItem item = lvListing.Items[index];
+            lvListing.Focus();
+            if (bringToTop)
+                lvListing.TopItem = item;
+            else
+                item.EnsureVisible();
+            item.Focused = true;
+            item.Selected = true;
+
+            // Update navigation history.
+                if (navCurrent == null)
+                {
+                    navCurrent = navHistory.AddFirst(index);
+                    return;
+                }
+
+            if (index == navCurrent.Value)
+                return;
+            if (navCurrent.Next != null && index == navCurrent.Next.Value)
+            {
+                navCurrent = navCurrent.Next;
+                return;
+            }
+
+            while (navCurrent.Next != null)
+            {
+                navHistory.RemoveLast();
+            }
+            navCurrent = navHistory.AddAfter(navCurrent, index);
+        }
+
+        
+    }
+}
