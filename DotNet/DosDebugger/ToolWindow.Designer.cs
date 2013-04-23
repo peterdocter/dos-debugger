b177a9ca41e2dcc0ae1295a259fc9644b5c1242d
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-04-23 10:38:17 +0000

    Enables the user to display closed, non-collapsed tool windows.

--- DotNet/DosDebugger/ToolWindow.Designer.cs
+++ DotNet/DosDebugger/ToolWindow.Designer.cs
@@ -40,6 +40,7 @@
             | WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) 
             | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.071428F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.HideOnClose = true;
             this.Name = "ToolWindow";
             this.TabText = "Tool Window";
             this.Text = "ToolWindow";
