f6dc1f61cb11d659f8081a506579dfc44deb4877
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-05-04 13:37:12 +0000

    Implement quick hack to display the disassembly of an OBJ module. However, this hack is extremely preliminary because 1) it assumes the segment with class name 'CODE' is fully filled with instructions, and 2) it ignores any fixup requirement.

--- trunk/DotNet/DosDebugger/Document.cs
+++ trunk/DotNet/DosDebugger/Document.cs
@@ -10,16 +10,22 @@ namespace DosDebugger
     {
         private Disassembler16 dasm;
         private NavigationPoint<Pointer> nav = new NavigationPoint<Pointer>();
+        private BinaryImage image;
 
         public Disassembler16 Disassembler
         {
-        //    get { return dasm; }
-            set { dasm = value; }
+            // get { return dasm; }
+            set
+            {
+                dasm = value;
+                image = dasm.Image;
+            }
         }
 
         public BinaryImage Image
         {
-            get { return dasm.Image; }
+            get { return image; }
+            set { image = value; }
         }
 
         public NavigationPoint<Pointer> Navigator
