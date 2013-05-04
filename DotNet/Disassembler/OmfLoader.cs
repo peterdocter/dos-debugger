140a187987924114cf1aef013be67bf32dc84058
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-05-04 06:45:26 +0000

    Parse more OMF record types.

--- DotNet/Disassembler/OmfLoader.cs
+++ DotNet/Disassembler/OmfLoader.cs
@@ -10,70 +10,106 @@ namespace Disassembler
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public class OmfLoader
     {
-        private Omf.Record[] records;
+        private ObjectModule[] modules;
 
-        public Omf.Record[] Records
+        public ObjectModule[] Modules
         {
             //get { return new ReadOnlyCollection<OmfRecord>(records); }
-            get { return records; }
+            get { return modules; }
         }
 
         public OmfLoader(string fileName)
         {
-            List<Omf.Record> records = new List<Omf.Record>();
-
             using (Stream stream = File.OpenRead(fileName))
             using (BinaryReader reader = new BinaryReader(stream))
             {
-                Omf.RecordContext context = new Omf.RecordContext();
-
-                // Read records.
-                int pageSize = 1;
-                while (stream.Position < stream.Length)
-                {
-                    Omf.Record record = Omf.Record.ReadRecord(reader, context);
-                    records.Add(record);
-
-                    if (record.RecordNumber == Omf.RecordNumber.LibraryHeader)
-                    {
-                        pageSize = ((Omf.LibraryHeaderRecord)record).PageSize;
-                    }
-                    else if (record.RecordNumber == Omf.RecordNumber.MODEND
-                          || record.RecordNumber == Omf.RecordNumber.MODEND32)
-                    {
-                        // This is the last record of an object module. Since
-                        // a LIB file consists of multiple object modules
-                        // aligned on page boundary, we need to consume the
-                        // padding bytes if present.
-                        int mod = (int)(stream.Position % pageSize);
-                        if (mod != 0)
-                        {
-                            reader.ReadBytes(pageSize - mod);
-                        }
-                    }
-                    else if (record.RecordNumber == Omf.RecordNumber.LibraryEnd)
-                    {
-                        break;
-                    }
-                }
-                this.records = records.ToArray();
-
-                // The dictionary follows, but we ignore it.
+                this.modules = LoadLibrary(reader);
             }
         }
 
-        public static ObjectModule LoadObject(Stream stream)
+        /// <summary>
+        /// Returns null if LibraryEnd record is encountered before
+        /// MODEND or MODEND32 record is encountered.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public static ObjectModule LoadObject(BinaryReader reader)
         {
-            throw new NotImplementedException();
+            ObjectModule module = new ObjectModule();
+            List<Omf.Record> records = new List<Omf.Record>();
+            Omf.RecordContext context = new Omf.RecordContext();
+
+            while (true)
+            {
+                Omf.Record record = Omf.Record.ReadRecord(reader, context);
+                records.Add(record);
+
+                if (record.RecordNumber == Omf.RecordNumber.MODEND ||
+                    record.RecordNumber == Omf.RecordNumber.MODEND32)
+                {
+                    break;
+                }
+                if (record.RecordNumber == Omf.RecordNumber.LibraryEnd)
+                {
+                    return null;
+                }
+            }
+            module.Records = records.ToArray();
+
+            // Convert the records to the information we need.
+            foreach (Omf.Record r in records)
+            {
+                if (r.RecordNumber == Omf.RecordNumber.THEADR)
+                {
+                    module.Name = ((Omf.TranslatorHeaderRecord)r).Name;
+                    break;
+                }
+            }
+
+            return module;
         }
 
-        public static ObjectModule[] LoadLibrary(Stream stream)
+        public static ObjectModule[] LoadLibrary(BinaryReader reader)
         {
-            throw new NotImplementedException();
+            List<ObjectModule> modules = new List<ObjectModule>();
+
+            Omf.LibraryHeaderRecord r = (Omf.LibraryHeaderRecord)
+                Omf.Record.ReadRecord(reader, null, Omf.RecordNumber.LibraryHeader);
+            int pageSize = r.PageSize;
+
+            while (true)
+            {
+                ObjectModule module = LoadObject(reader);
+                if (module == null) // LibraryEndRecord encountered
+                {
+                    break;
+                }
+                modules.Add(module);
+
+                // Since a LIB file consists of multiple object modules
+                // aligned on page boundary, we need to consume the padding
+                // bytes if present.
+                int mod = (int)(reader.BaseStream.Position % pageSize);
+                if (mod != 0)
+                {
+                    reader.ReadBytes(pageSize - mod);
+                }
+            }
+
+            // The dictionary follows, but we ignore it.
+            return modules.ToArray();
         }
     }
 
+    [TypeConverter(typeof(ExpandableObjectConverter))]
     public class ObjectModule
     {
+        public string Name { get; internal set; }
+        public Omf.Record[] Records { get; internal set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
     }
 }
