3e2228b41f55bc817693bc48b283ddf927e63eff
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-05-10 07:42:01 +0000

    WPF: Remember docking layout when closing the main window.

--- trunk/DotNet/WpfDebugger/MainWindow.xaml.cs
+++ trunk/DotNet/WpfDebugger/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Disassembler;
+using AvalonDock.Layout;
 
 namespace WpfDebugger
 {
@@ -24,16 +25,18 @@ namespace WpfDebugger
         public MainWindow()
         {
             InitializeComponent();
+
+            // Try load the last layout.
+            try
+            {
+                LoadDockingLayout();
+            }
+            catch (Exception)
+            {
+            }
         }
 
         BinaryImage image;
-        //ListingViewModel viewModel;
-
-        private void btnTest_Click(object sender, RoutedEventArgs e)
-        {
-            //lvData.ItemsSource = viewModel.Rows;
-            //procedureList.Image = image;
-        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -47,10 +50,144 @@ namespace WpfDebugger
             //this.viewModel = new ListingViewModel(dasm.Image);
         }
 
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveDockingLayout();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        #region Docking Layout Save/Load
+
+        private void SaveDockingLayout()
+        {
+            var serializer = new AvalonDock.Layout.Serialization.XmlLayoutSerializer(
+                dockingManager);
+            using (var stream = System.IO.File.Create("AvalonLayoutConfig.xml"))
+            {
+                serializer.Serialize(stream);
+            }
+        }
+
+        private void LoadDockingLayout()
+        {
+            var serializer = new AvalonDock.Layout.Serialization.XmlLayoutSerializer(
+                dockingManager);
+            //serializer.LayoutSerializationCallback += serializer_LayoutSerializationCallback;
+            using (var stream = System.IO.File.OpenRead("AvalonLayoutConfig.xml"))
+            {
+                serializer.Deserialize(stream);
+            }
+        }
+
+        private void mnuFileSaveLayout_Click(object sender, RoutedEventArgs e)
+        {
+            SaveDockingLayout();
+            MessageBox.Show("Layout saved.");
+        }
+
+        private void mnuFileLoadLayout_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDockingLayout();
+        }
+
+        #endregion
+
         private void mnuHelpTest_Click(object sender, RoutedEventArgs e)
         {
             this.disassemblyList.Image = image;
             this.procedureList.Image = image;
+            this.errorList.Image = image;
         }
+
+        private void mnuFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #region Tool Window Activation
+
+        private void mnuViewErrors_Click(object sender, RoutedEventArgs e)
+        {
+            ActivateToolWindow(errorList);
+        }
+
+        private void mnuViewProcedures_Click(object sender, RoutedEventArgs e)
+        {
+            ActivateToolWindow(procedureList);
+        }
+
+        /// <summary>
+        /// Activates the dockable pane that contains the given control.
+        /// The search is performed by matching the pane's ContentId to the
+        /// controls's Name. If no dockable pane contains the control, one is
+        /// created at the bottom side of the docking root; in this case, the
+        /// control's ToolTip (if it is a non-null string) is used as the
+        /// pane's Title.
+        /// </summary>
+        /// <param name="control">The control to activate.</param>
+        /// <remarks>
+        /// This code is partly adapted from AvalonDock samples. It's not
+        /// clear how it's done, but normally it works.
+        /// </remarks>
+        private void ActivateToolWindow(Control control)
+        {
+            if (control == null)
+                throw new ArgumentNullException("control");
+
+            string contentId = control.Name;
+
+            LayoutAnchorable pane = dockingManager.Layout.Descendents().OfType<
+                LayoutAnchorable>().SingleOrDefault(a => a.ContentId == contentId);
+
+            if (pane == null)
+            {
+                // The pane is not created. This can happen for example when
+                // we load from an old layout configuration file, and the
+                // pane is not defined in that file. In this case, we add the
+                // control to a default location.
+                var anchorSide = dockingManager.BottomSidePanel.Model as LayoutAnchorSide;
+                LayoutAnchorGroup anchorGroup;
+                if (anchorSide.ChildrenCount == 0)
+                {
+                    anchorGroup = new LayoutAnchorGroup();
+                    anchorSide.Children.Add(anchorGroup);
+                }
+                else
+                {
+                    anchorGroup = anchorSide.Children[0];
+                }
+
+                pane = new LayoutAnchorable();
+                pane.ContentId = contentId;
+                pane.Content = control;
+                if (control.ToolTip is string)
+                {
+                    pane.Title = (string)control.ToolTip;
+                }
+                anchorGroup.Children.Add(pane);
+            }
+
+            if (pane.IsHidden)
+            {
+                pane.Show();
+            }
+            else if (pane.IsVisible)
+            {
+                pane.IsActive = true;
+            }
+            else
+            {
+                pane.AddToLayout(dockingManager,
+                    AnchorableShowStrategy.Bottom |
+                    AnchorableShowStrategy.Most);
+            }
+        }
+
+        #endregion
     }
 }
