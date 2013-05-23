cef4bbabc47f0df0a52326779041f520a4c8642b
fancitron@gmail.com <fancitron@gmail.com@d74d4715-becb-e380-c6d0-a3abd44de5ef>
2013-05-23 01:00:53 +0000

    Make XRef use ResolvedAddress instead of LogicalAddress.

--- trunk/DotNet/Disassembler/Error.cs
+++ trunk/DotNet/Disassembler/Error.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace Disassembler2
 {
@@ -11,10 +12,10 @@ namespace Disassembler2
     {
         public ErrorCode ErrorCode { get; private set; }
         public ErrorCategory Category { get; private set; }
-        public LogicalAddress Location { get; private set; }
+        public ResolvedAddress Location { get; private set; }
         public string Message { get; private set; }
 
-        public Error(LogicalAddress location, ErrorCode errorCode, string message)
+        public Error(ResolvedAddress location, ErrorCode errorCode, string message)
         {
             this.Category = ErrorCategory.Error;
             this.Location = location;
@@ -22,21 +23,21 @@ namespace Disassembler2
             this.ErrorCode = errorCode;
         }
 
-        public Error(LogicalAddress location, string message, ErrorCategory category)
+        public Error(ResolvedAddress location, string message, ErrorCategory category)
         {
             this.Category = category;
             this.Location = location;
             this.Message = message;
         }
 
-        public Error(LogicalAddress location, string message)
+        public Error(ResolvedAddress location, string message)
             : this(location, message, ErrorCategory.Error)
         {
         }
 
         public static int CompareByLocation(Error x, Error y)
         {
-            return LogicalAddress.CompareByLexical(x.Location, y.Location);
+            return ResolvedAddress.CompareByLexical(x.Location, y.Location);
         }
     }
 
@@ -47,6 +48,16 @@ namespace Disassembler2
         Error = 1,
         Warning = 2,
         Message = 4,
+    }
+
+    public class ErrorCategoryAttribute : Attribute
+    {
+        readonly ErrorCategory category;
+
+        public ErrorCategoryAttribute(ErrorCategory category)
+        {
+            this.category = category;
+        }
     }
 
     public enum ErrorCode
@@ -62,6 +73,11 @@ namespace Disassembler2
         /// returned with RETN and a far call with RETF, this error indicates
         /// a potential problem with the analysis.
         /// </summary>
+        /// [Summary]
+        /// [Description]
+        /// [Example]
+        /// [Solution]
+        /// 
         InconsistentCall,
 
         /// <summary>
@@ -73,6 +89,40 @@ namespace Disassembler2
         /// Indicates that we ran into the middle of an instruction.
         /// </summary>
         RanIntoCode,
+
+        /// <summary>
+        /// While analyzing a basic block, a decoded instruction would 
+        /// overlap with existing bytes that are already analyzed as code
+        /// or data.
+        /// </summary>
+        /// <remarks>
+        /// Possible causes for this error include:
+        /// - 
+        /// - Other analysis errors.
+        /// </remarks>
+        [ErrorCategory(ErrorCategory.Error)]
+        OverlappingInstruction,
+
+        /// <summary>
+        /// After executing an instruction, the CS:IP pointer would wrap
+        /// around 0xFFFF.
+        /// </summary>
+        /// <remarks>
+        /// While it is technically allowed (and occassionally useful) to let
+        /// CS:IP wrap, this situation typically indicates an analysis error.
+        /// </remarks>
+        [ErrorCategory(ErrorCategory.Error)]
+        AddressWrapped,
+
+        /// <summary>
+        /// The target of a branch/call/jump instruction cannot be determined
+        /// through static analysis.
+        /// </summary>
+        /// <remarks>
+        /// To resolve this problem, dynamic analysis can be employed.
+        /// </remarks>
+        [ErrorCategory(ErrorCategory.Message)]
+        DynamicTarget,
     }
 
     public class ErrorCollection : List<Error>
